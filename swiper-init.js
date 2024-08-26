da5a614fca0e72adbba5c7d1a9500d877e79c7cb
Pedro Robalo <35338193+PedroRobalo1994@users.noreply.github.com>
2024-08-26 20:34:11 +0100

    chore: Add Swiper carousel with responsive breakpoints

---
+++ swiper-init.js
@@ -0,0 +1,22 @@
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 40,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        600: {
+          slidesPerView: 1,
+          spaceBetween: 40,
+        },
+        800: {
+          slidesPerView: 3,
+          spaceBetween: 40,
+        },
+      },
+  });
